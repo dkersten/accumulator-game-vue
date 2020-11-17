420fff231a3a8ff7a168b141415bd12c614e2a63
dkersten <dkersten1994@gmail.com>
2020-11-17 18:32:23 -0500

    refactored inner stats container into its own component

---
+++ src/components/InnerStatsContainer.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="inner-stats-container">
+        <p>Your Wealth: $<span class="wealth-count-num">100</span></p>
+        <p>Per Second: $<span class="per-second-num">0</span></p>
+        <p>Total Net Worth: $<span class="net-worth-num">0</span></p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "InnerStatsContainer",
+    components: {
+
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+    @import '../assets/scss/variables.scss';
+    @import '../assets/scss/mixins.scss';
+
+    .inner-stats-container {
+        text-align: center;
+        font-size: $font-paragraph-sm;
+
+        p {
+            &:not(:last-of-type) {
+                margin-bottom: $vertical-spacing-sxs;
+            }
+
+            span {
+                font-weight: 700;
+
+                &.wealth-count-num {
+                    color: $color-green-dark;
+                }
+
+                &.per-second-num, &.net-worth-num {
+                    color: $color-green-bright;
+                }
+            }
+        }
+    }
+
+</style>
--- src/containers/StatsContainer.vue
+++ src/containers/StatsContainer.vue
@@ -1,19 +1,18 @@
 <template>
   <section class="stats-container">
       <img src="../../public/dollarBill.png" alt="Dollar Bill. Click it to make more money.">
-      <div class="inner-stats-container">
-          <p>Your Wealth: $ <span class="wealth-count-num">100</span></p>
-          <p>Per Second: $ <span class="per-second-num">0</span></p>
-          <p>Total Net Worth: $ <span class="net-worth-num">0</span></p>
-      </div>
+      <InnerStatsContainer></InnerStatsContainer>
   </section>
 </template>
 
 <script>
+
+import InnerStatsContainer from '../components/InnerStatsContainer'
+
 export default {
     name: 'StatsContainer',
     components: {
-
+        InnerStatsContainer
     }
 }
 </script>
@@ -29,30 +28,6 @@ export default {
         img {
             display: block;
             margin: 0 auto $vertical-spacing-sm;
-        }
-
-        .inner-stats-container {
-
-            text-align: center;
-
-            p {
-                &:not(:last-of-type) {
-                    margin-bottom: $vertical-spacing-sxs;
-                }
-
-                span {
-                    font-weight: 700;
-                    font-size: $font-paragraph-lg;
-
-                    &.wealth-count-num {
-                        color: $color-green-dark;
-                    }
-
-                    &.per-second-num, &.net-worth-num {
-                        color: $color-green-bright;
-                    }
-                }
-            }
         }
     }
 </style>
