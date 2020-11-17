f18c2ec25f373224300e1673e632a2cc734338c9
dkersten <dkersten1994@gmail.com>
2020-11-17 18:27:38 -0500

    worked on stats container section

--- src/containers/StatsContainer.vue
+++ src/containers/StatsContainer.vue
@@ -1,6 +1,11 @@
 <template>
   <section class="stats-container">
-      <p>Dollar Here</p>
+      <img src="../../public/dollarBill.png" alt="Dollar Bill. Click it to make more money.">
+      <div class="inner-stats-container">
+          <p>Your Wealth: $ <span class="wealth-count-num">100</span></p>
+          <p>Per Second: $ <span class="per-second-num">0</span></p>
+          <p>Total Net Worth: $ <span class="net-worth-num">0</span></p>
+      </div>
   </section>
 </template>
 
@@ -20,5 +25,34 @@ export default {
 
     .stats-container {
         @include section-styling;
+
+        img {
+            display: block;
+            margin: 0 auto $vertical-spacing-sm;
+        }
+
+        .inner-stats-container {
+
+            text-align: center;
+
+            p {
+                &:not(:last-of-type) {
+                    margin-bottom: $vertical-spacing-sxs;
+                }
+
+                span {
+                    font-weight: 700;
+                    font-size: $font-paragraph-lg;
+
+                    &.wealth-count-num {
+                        color: $color-green-dark;
+                    }
+
+                    &.per-second-num, &.net-worth-num {
+                        color: $color-green-bright;
+                    }
+                }
+            }
+        }
     }
 </style>
